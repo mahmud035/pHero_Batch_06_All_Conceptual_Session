4a430984fae272e9580b8df34b37798c145e1908
mahmud035 <mahamudulhasan4148@gmail.com>
2023-03-25 14:49:16 +0600

    session done

--- Milestone-8-React-Core-Concepts/src/App.jsx
+++ Milestone-8-React-Core-Concepts/src/App.jsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div>
-      <h1 className="text-5xl font-bold text-center underline">Vite + React</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

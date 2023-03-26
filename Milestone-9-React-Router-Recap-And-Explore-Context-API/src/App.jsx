4088e6ca8e6e51ba0cb23a9cb8534e709684ca66
mahmud035 <mahamudulhasan4148@gmail.com>
2023-03-26 14:41:34 +0600

    basic set up for this session

---
+++ Milestone-9-React-Router-Recap-And-Explore-Context-API/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <h1 className="text-5xl">Welcome to Smart Home Starter Template</h1>
+    </div>
+  );
+}
+
+export default App;

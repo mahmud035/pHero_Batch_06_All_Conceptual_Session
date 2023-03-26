e15332226a8127ba885f5671d9c4ebb69aabefe3
mahmud035 <mahamudulhasan4148@gmail.com>
2023-03-26 06:04:10 +0600

    change loader function from multiple line to one line

--- Milestone-9-Deep-Dive-Into-React-Router-Dom/src/routes/Routes.jsx
+++ Milestone-9-Deep-Dive-Into-React-Router-Dom/src/routes/Routes.jsx
@@ -27,16 +27,13 @@ const router = createBrowserRouter([
       {
         path: '/books',
         element: <Books />,
-        loader: async () => {
-          return fetch('https://api.itbook.store/1.0/new');
-        },
+        loader: () => fetch('https://api.itbook.store/1.0/new'),
       },
       {
         path: '/books/:isbn13',
         element: <BookDetails />,
-        loader: async ({ params }) => {
-          return fetch(`https://api.itbook.store/1.0/books/${params.isbn13}`);
-        },
+        loader: ({ params }) =>
+          fetch(`https://api.itbook.store/1.0/books/${params.isbn13}`),
       },
     ],
   },

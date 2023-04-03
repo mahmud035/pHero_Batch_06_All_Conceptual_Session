af1212dc8a427ea6245b4204b4d1a98680727d9f
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-04 03:24:13 +0600

    initial setup for this project

---
+++ Milestone-10-React-Authentication-With-Toggle-State-Private-Route/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className="flex justify-center items-center min-h-screen">
+        <h1 className="text-3xl">Welcome to Authentech</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;

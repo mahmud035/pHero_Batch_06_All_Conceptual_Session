3ab37e95527ca36e1ab0e904baedafbbb094b441
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-04 06:10:27 +0600

    initialize firabase and Basic UserContext setup

---
+++ Milestone-10-React-Authentication-With-Toggle-State-Private-Route/src/firebase/firebase.config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBqs37RbHh_XxVTXFggVQ8-fSzwj1sFUfY',
+  authDomain: 'authentech-conceptual.firebaseapp.com',
+  projectId: 'authentech-conceptual',
+  storageBucket: 'authentech-conceptual.appspot.com',
+  messagingSenderId: '651960104070',
+  appId: '1:651960104070:web:a6f678c0010e839b04cd07',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

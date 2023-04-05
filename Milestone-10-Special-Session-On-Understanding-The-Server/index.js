b7429e6c83454a3c8beadc8a149759968863075a
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-05 18:11:00 +0600

    get all products and get specific product

--- Milestone-10-Special-Session-On-Understanding-The-Server/index.js
+++ Milestone-10-Special-Session-On-Understanding-The-Server/index.js
@@ -5,6 +5,30 @@ app.use(cors());
 
 const Port = process.env.PORT || 5000;
 
+const productsCollection = require('./data/products.json');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    res.send(productsCollection);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const product = productsCollection.find((p) => p.id === id);
+    res.send(product);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 app.listen(Port, () => {
   console.log('Server is running:', Port);
 });

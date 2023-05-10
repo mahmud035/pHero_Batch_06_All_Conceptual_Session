51e7fd72030adf3a724500191db03b8f66ab9d4b
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-11 04:26:44 +0600

    create Database Collections

--- Milestone-11-Master-in-CRUD-Operation/cs-crud-with-mongodb-server/index.js
+++ Milestone-11-Master-in-CRUD-Operation/cs-crud-with-mongodb-server/index.js
@@ -14,13 +14,17 @@ const dbConnect = async () => {
   try {
     await client.connect();
     console.log(`Database Connected`.yellow.italic);
-
-    // undefined.null();
   } catch (error) {
     console.log(error.name.bgRed, error.message.bold, error.stack);
   }
 };
 dbConnect();
+
+//* Database Collections
+const Products = client.db('curdOperation').collection('products');
+const Users = client.db('crudOperation').collection('users');
+
+Products.insertOne({ name: 'John' });
 
 app.listen(PORT, () => {
   console.log(`Server Up and Running`.cyan.blue);

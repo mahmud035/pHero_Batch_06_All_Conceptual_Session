80148ab2eacad113260d616321be9bce604d6489
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-11 08:44:32 +0600

    get all products data using TanStack Query

--- Milestone-11-Master-in-CRUD-Operation/cs-crud-with-mongodb/src/components/Dashboard/AllProducts.js
+++ Milestone-11-Master-in-CRUD-Operation/cs-crud-with-mongodb/src/components/Dashboard/AllProducts.js
@@ -1,10 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
 
 const AllProducts = () => {
-  return (
-    <div>
-      All products here
-    </div>
-  );
+  const url = `http://localhost:5000/products`;
+  const {
+    isLoading,
+    isError,
+    data: products = [],
+    error,
+  } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data.data;
+    },
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  console.log(products);
+
+  return <div>All products here</div>;
 };
 
 export default AllProducts;

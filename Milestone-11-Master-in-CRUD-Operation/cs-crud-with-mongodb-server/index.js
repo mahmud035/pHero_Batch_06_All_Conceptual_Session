0c2a2ffb6b9b8f7f0a437963dc8019736f6e99f7
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-11 06:26:17 +0600

    test GET and POST request and send response with a specific object property of success and message

--- Milestone-11-Master-in-CRUD-Operation/cs-crud-with-mongodb-server/index.js
+++ Milestone-11-Master-in-CRUD-Operation/cs-crud-with-mongodb-server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
+const cors = require('cors');
 require('colors');
 
 const PORT = process.env.PORT || 5000;
 
+//* Middleware
+app.use(cors());
+app.use(express.json());
+
+//* Connect with MongoDB
 const uri =
   'mongodb+srv://crudOperation:GByKcm9sNaU4XJcU@cluster0.yeflywl.mongodb.net/?retryWrites=true&w=majority';
 
@@ -20,11 +26,64 @@ const dbConnect = async () => {
 };
 dbConnect();
 
-//* Database Collections
+//* Create Database Collections
 const Products = client.db('curdOperation').collection('products');
 const Users = client.db('crudOperation').collection('users');
 
-Products.insertOne({ name: 'John' });
+// ============ API REQUEST ============
+//* GET
+app.get('/products', async (req, res) => {
+  try {
+    res.send('Here is your data');
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold, error.stack);
+
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+//* POST
+app.post('/products', async (req, res) => {
+  try {
+    const product = req.body;
+    const result = await Products.insertOne(product);
+    // console.log(result);
+
+    undefined.null();
+
+    if (result.insertedId) {
+      res.send({
+        success: true,
+        message: `Successfully created the product`,
+      });
+    } else {
+      res.send({
+        success: false,
+        message: `Failed to create the product`,
+      });
+    }
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold, error.stack);
+
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+//* PUT / PATCH
+
+//* DELETE
+
+//* Global Error Handling
+app.use((error, req, res, next) => {
+  console.log(error.name.bgRed, error.message.bold, error.stack);
+  res.status(500).send(`Something broke`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server Up and Running`.cyan.blue);
